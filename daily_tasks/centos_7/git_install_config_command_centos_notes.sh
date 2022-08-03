34dd2a00a9c13b9540c8b17c4f6f425b413486ad
nima-yeganeh <yeganeh.nima.d@gmail.com>
2022-08-03 14:11:29 +0430

    done

---
+++ daily_tasks/centos_7/git_install_config_command_centos_notes.sh
@@ -0,0 +1,57 @@
+sudo yum update
+ifconfig
+top
+sudo yum update
+sudo dnf install git
+sudo yum install git
+sudo yum install dnf
+sudo dnf install git-all
+git --version
+pwd
+ls -anp
+mkdir nima_git
+cd nima_git/
+ls -anp
+sudo dnf install git-all
+git --help
+git config --global user.name 'your_git_username'
+git config --global user.email 'your_git_email_address'
+git config --list
+git clone https://github.com/Nima-Yeganeh/Nima-Yeganeh
+ls -anp
+cd Nima-Yeganeh/
+git status
+git pull
+git push
+cd ..
+cd Downloads/
+ls -anp
+sudo dnf install rpm
+sudo yum install alien
+sudo yum install tar
+#https://git-scm.com/book/en/v2/Getting-Started-Installing-Git
+#https://git-scm.com/download/linux
+#https://github.com/GitCredentialManager/git-credential-manager/releases/tag/v2.0.785
+#https://linuxconfig.org/how-to-install-deb-file-in-redhat-linux-8
+#https://github.com/GitCredentialManager/git-credential-manager
+tar -xvf gcm-linux_amd64.2.0.785.tar.gz -C /usr/local/bin/
+sudo tar -xvf gcm-linux_amd64.2.0.785.tar.gz -C /usr/local/bin/
+git-credential-manager-core --help
+git-credential-manager-core configugre
+git-credential-manager-core configure
+cd ..
+ls -anp
+cd nima_git/
+ls -anp
+cd Nima-Yeganeh/
+ls -anp
+git pull
+git push
+git config --global credential.credentialStore secretservice
+git pull
+git push
+git add .
+git commit -m 'done'
+git status
+git push
+git status
