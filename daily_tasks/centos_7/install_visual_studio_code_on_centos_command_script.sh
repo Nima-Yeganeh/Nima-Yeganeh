c034f86a11d640ff1ecd37b156e1ebf8bc35ef25
nima-yeganeh <yeganeh.nima.d@gmail.com>
2022-08-05 15:29:32 +0430

    done

--- daily_tasks/centos_7/install_visual_studio_code_on_centos_command_script.sh
+++ daily_tasks/centos_7/install_visual_studio_code_on_centos_command_script.sh
@@ -2,3 +2,6 @@ sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo cp -f vscode.repo /etc/yum.repos.d/vscode.repo
 sudo cat /etc/yum.repos.d/vscode.repo
 sudo yum install -y code
+sudo yum update -y
+sudo yum upgrade -y
+
