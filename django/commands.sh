85e7c8547b8c5fd6f0fed7b02338a5919086afbd
nima-yeganeh <yeganeh.nima.d@gmail.com>
2022-08-09 08:08:58 -0700

    done

---
+++ django/commands.sh
@@ -0,0 +1,16 @@
+sudo apt update -y
+sudo apt install python3 -y
+sudo apt install python3-pip -y
+python3 -V
+pip --version
+sudo apt install -y python3-venv
+python3 -m pip install django~=4.0.0
+python3 -m pip install --upgrade pip
+sudo apt install -y python3-django
+django-admin startproject django_project .
+sudo apt install -y tree
+tree .
+ls -anp
+python3 manage.py runserver
+
+
