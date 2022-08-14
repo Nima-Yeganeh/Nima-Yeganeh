4bc80e4ff792576192e6b57f79b68e3a07537662
nima-yeganeh <yeganeh.nima.d@gmail.com>
2022-08-14 23:16:45 +0430

    done

--- daily_tasks/ubuntu_22/docker_install_commands_ubuntu.sh
+++ daily_tasks/ubuntu_22/docker_install_commands_ubuntu.sh
@@ -1,16 +1,12 @@
 sudo apt-get remove -y docker docker-engine docker.io containerd runc
+
 sudo apt-get update -y
-sudo apt-get install -y \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
+sudo apt-get install -y ca-certificates curl gnupg lsb-release
 sudo apt install -y python3 python3-pip
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+cat /etc/apt/sources.list.d/docker.list
 sudo apt-get update -y
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
