c4264a76214d0a22479ccb3b10c1f51aad611e7d
root <yeganeh.nima.d@gmail.com>
2022-08-18 21:27:51 +0430

    done

--- gitlab/how-to2/install-gitlab-server-with-docker-notes-commands.sh
+++ gitlab/how-to2/install-gitlab-server-with-docker-notes-commands.sh
@@ -40,17 +40,48 @@ networks:
 # volumes – specifies the volumes that are used by the container. In our configuration, we have directories shared with our system (subdirectories in $ GITLAB_HOME) and an additional volume that allows access to the Docker environment from the GitLab runner.
 # networks – defines the virtual network in which the containers will operate. In our case, the www portal and the runner operate in one “gitlab-network”
 
-
-
-# commands >>
+# Gitlab installation
+# Containers are started using the command:
 docker-compose up -d
-docker logs -f gitlab-nima-tutorial
+
+# To log in to GitLab for the first time, you need a temporary password, which is generated automatically during installation. We get the password using the command:
+docker exec -it gitlab-ce grep 'Password:' /etc/gitlab/initial_root_password
+docker exec -it gitlab-ce cat /etc/gitlab/initial_root_password
+
+# GitLab launching
+# Our GitLab is available at: http: //localhost:8080
+# To log in to the portal, we must enter the “root” in Username field and the temporary password that we obtained earlier in the Password field.
+
+# GitLab runner configuration
+# To use the GitLab runner in GitLab, you need to configure it. For correct configuration, we will need a token copied from the portal. # To do this, go to the address: http://localhost:8080/admin/runners and click the Copy token button.
+# In the next step, it goes to the console and run the following command:
+docker exec -it gitlab-runner gitlab-runner register --url "http://gitlab-ce" --clone-url "http://gitlab-ce"
+
+# After launching, a configuration module will appear. The module provides the following information:
+# Enter the GitLab instance URL: confirm the entered value (click enter)
+# Enter the registration token: enter the token copied before.
+# Enter a description for the runner: enter the name of the runner, e.g. docker-runner
+# Enter tags for the runner: leave the field blank here
+# Enter an executor: enter docker here
+# Enter the default Docker image: here we provide the default docker image, e.g. maven: latest
+# After proper configuration, we should see confirmation Runner registred successfully:
+
+# In addition to the basic configuration, we also need to allow access for containers launched from the runner to the virtual network in which GitLab operates. To do this, we run the editor (e.g. vi)
+sudo vi gitlab/gitlab-runner/config.toml
+# Then we add new line to the end of the runner configuration: network_mode = “gitlab-network”
+
+# To check if the runner is available from the GitLab level, go to the following address:http://localhost:8080/admin/runners
+
+# docker commands >>
+docker-compose up -d
+docker-compose down
+docker logs -f gitlab-ce
 docker-ps
-docker stop gitlab-nima-tutorial
-docker start gitlab-nima-tutorial
-docker container rm gitlab-nima-tutorial
-docker stop gitlab-nima-tutorial
-docker rm gitlab-nima-tutorial
+docker stop gitlab-ce
+docker start gitlab-ce
+docker container rm gitlab-ce
+docker stop gitlab-ce
+docker rm gitlab-ce
 docker pull gitlab/gitlab-ee:latest
 docker-compose up -d
-docker exec -it gitlab-nima-tutorial cat /etc/gitlab/initial_root_password
+docker exec -it gitlab-ce cat /etc/gitlab/initial_root_password
