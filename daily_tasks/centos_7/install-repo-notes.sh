7f919712725427fd1638bb8ac4d7af944f4e3f2c
root <yeganeh.nima.d@gmail.com>
2022-08-22 12:53:26 +0430

    done

---
+++ daily_tasks/centos_7/install-repo-notes.sh
@@ -0,0 +1,9 @@
+# CentOS 7.7 NetInstall URL (Installation Source)
+# http://mirrors.sonic.net/centos/7/os/x86_64/
+# http://mirrors.sonic.net/centos/7.7.1908/os/x86_64/
+
+# Alternatively use CentOS own mirror
+# http://mirror.centos.org/centos/7/os/x86_64/
+# http://mirror.centos.org/centos/7.7.1908/os/x86_64/
+
+# https://mirrors.sonic.net/centos/7.9.2009/os/x86_64/
