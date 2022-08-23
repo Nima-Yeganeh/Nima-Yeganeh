d3cdedb7b6760c1e37d4b0bf48ed07f6b66bd6ef
root <yeganeh.nima.d@gmail.com>
2022-08-23 15:37:31 +0430

    done

--- docker/notes-and-commands.sh
+++ docker/notes-and-commands.sh
@@ -23,3 +23,44 @@ docker service ls
 
 cat /etc/docker/daemon.json
 docker system info
+sudo docker system info | grep overlay
+
+# direct-lvm
+sudo vi /etc/docker/daemon.json
+{
+"storage-driver": "devicemapper",
+"storage-opts": [
+"dm.directlvm_device=/dev/xdf",
+"dm.thinp_percent=95",
+"dm.thinp_metapercent=1",
+"dm.thinp_autoextend_threshold=80",
+"dm.thinp_autoextend_percent=20",
+"dm.directlvm_device_force=false"
+]
+}
+
+# /var/run/docker.sock
+
+docker image ls
+docker image pull ubuntu:latest
+docker image pull microsoft/powershell:nanoserver
+docker images
+
+docker container run -it ubuntu:latest /bin/bash
+docker container run -it microsoft/powershell:nanoserver pwsh.exe
+
+ps -elf
+ps
+
+docker container ls
+docker container exec -it vigilant_borg bash
+docker container start
+
+docker container exec -it pensive_hamilton pwsh.exe
+
+docker container stop vigilant_borg
+docker container rm vigilant_borg
+
+docker container ls -a
+
+
