d685cd8cc3c7a128c324995f9facf5df1aa1f3ef
root <yeganeh.nima.d@gmail.com>
2022-08-25 15:18:33 +0430

    done

--- aws/net_performance_check_wget.sh
+++ aws/net_performance_check_wget.sh
@@ -1,5 +1,4 @@
 for ((i=1; i<200; i++));
 do
-    wget google.com -O /dev/null -q --no-dns-cache --no-cache &&
-    
+    wget google.com/$i.html -O /dev/null -q --no-dns-cache --no-cache && sleep 0
 done
