3c9f6ee6f17f2fb51f593a443e0eff82f738882f
root <yeganeh.nima.d@gmail.com>
2022-08-25 15:28:27 +0430

    done

--- aws/net_performance_check_wget.sh
+++ aws/net_performance_check_wget.sh
@@ -4,5 +4,8 @@ do
     num2=$(shuf -i 1-240 -n 1)
     num3=$(shuf -i 1-240 -n 1)
     num4=$(shuf -i 1-240 -n 1)
-    wget google.com -O /dev/null -q --no-dns-cache --no-cache && sleep 0 && wget $num1.$num2.$num3.$num4 -O /dev/null -q --no-dns-cache --no-cache && sleep 0
+    wget google.com -O /dev/null -q --no-dns-cache --no-cache && 
+    wget $num1.$num2.$num3.$num4 -O /dev/null -q --no-dns-cache --no-cache && 
+    sleep 0
+    ps aux | grep wget
 done
