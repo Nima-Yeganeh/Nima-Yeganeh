0316f70af795d856760cb093faadec8b694af573
root <yeganeh.nima.d@gmail.com>
2022-08-25 15:42:47 +0430

    done

--- aws/ubuntu_update.sh
+++ aws/ubuntu_update.sh
@@ -1,4 +1,5 @@
 # git clone https://github.com/Nima-Yeganeh/Nima-Yeganeh.git
+
 git pull
 sudo apt update -y
 sudo apt install puppet -y
@@ -9,3 +10,16 @@ sudo apt install -y java*
 sudo apt install -y iotop iftop screen
 sudo apt install -y node*
 ls -anp
+sudo docker run -d ubuntu
+sudo docker run -d yeganehnimadgmailcom/myapache2
+sudo docker run -d yeganehnimadgmailcom/myapache2test2
+sudo docker run -d yeganehnimadgmailcom/python-flask-docker-cicd-helloworld-test
+sudo docker run -d yeganehnimadgmailcom/my-app
+sudo docker run -d yeganehnimadgmailcom/hello-world-express-js-node-test
+sudo docker run -d yeganehnimadgmailcom/docker101tutorial
+sudo docker run -d yeganehnimadgmailcom/docker_test1
+sudo docker run -d yeganehnimadgmailcom/docker-test-psweb2
+sudo docker run -d debian
+sudo docker run -d alpine
+sudo docker ps -a
+sudo docker images
