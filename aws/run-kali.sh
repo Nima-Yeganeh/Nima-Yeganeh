27789c9a189b56c180b1fae68ae8c636565e8c63
root <yeganeh.nima.d@gmail.com>
2022-08-26 18:06:00 +0430

    done

--- aws/run-kali.sh
+++ aws/run-kali.sh
@@ -1,4 +1,5 @@
-sudo docker pull kalilinux/kali-linux-docker
+sudo docker pull docker.io/kalilinux/kali-rolling
+# sudo docker pull kalilinux/kali-linux-docker
 sudo docker images
 sudo docker ps -a
-sudo docker run -it kalilinux/kali-linux-docker
+sudo docker run -it docker.io/kalilinux/kali-rolling
