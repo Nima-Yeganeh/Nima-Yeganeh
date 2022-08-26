443dbe610ac42a0811976cee2e0d119b0dbe820f
root <yeganeh.nima.d@gmail.com>
2022-08-26 20:29:30 +0430

    done

--- aws/kali-full-docker/do.sh
+++ aws/kali-full-docker/do.sh
@@ -1,8 +1,9 @@
 sudo docker image rm kali-linux-full
 sudo docker image rm yeganehnimadgmailcom/kali-linux-full
-sudo docker image build -t kali-linux-full .
+# sudo docker image build -t kali-linux-full .
+sudo docker image build -t yeganehnimadgmailcom/kali-linux-full .
 sudo docker images
 sudo docker image ls | grep kali-linux-full
-sudo docker image tag kali yeganehnimadgmailcom/kali-linux-full
+# sudo docker image tag kali yeganehnimadgmailcom/kali-linux-full
 sudo docker images | grep kali-linux-full
 # sudo docker image push yeganehnimadgmailcom/kali-linux-full
