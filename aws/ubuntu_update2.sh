ec1f000b4fe4d1fa25e5fedb51de14819f7bea63
root <yeganeh.nima.d@gmail.com>
2022-08-26 17:48:28 +0430

    done

---
+++ aws/ubuntu_update2.sh
@@ -0,0 +1,15 @@
+# git clone https://github.com/Nima-Yeganeh/Nima-Yeganeh.git
+
+git pull
+sudo apt update -y
+sudo apt install puppet -y
+sudo apt install docker docker.io -y
+sudo apt install git
+sudo apt install -y python python3 python3-pip
+sudo apt install -y iotop iftop screen
+ls -anp
+sudo docker run -d ubuntu
+sudo docker run -d debian
+sudo docker run -d alpine
+sudo docker ps -a
+sudo docker images
