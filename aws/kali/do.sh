746ea6e2fc97d099ff2789a8465b389509ba645b
root <yeganeh.nima.d@gmail.com>
2022-08-26 18:27:56 +0430

    done

---
+++ aws/kali/do.sh
@@ -0,0 +1,6 @@
+sudo docker image build -t kali .
+sudo docker images
+sudo docker image ls | grep kali
+sudo docker image tag kali yeganehnimadgmailcom/kali
+sudo docker images | grep kali
+sudo docker image push yeganehnimad/yeganehnimadgmailcom
