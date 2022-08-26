7450a99d800b62920d34ec938ae7048cbc7e70f6
root <yeganeh.nima.d@gmail.com>
2022-08-26 21:11:10 +0430

    done

--- aws/update.sh
+++ aws/update.sh
@@ -8,8 +8,8 @@ sudo apt install git
 sudo apt install -y python python3 python3-pip
 sudo apt install -y iotop iftop screen
 ls -anp
-sudo docker run -d ubuntu
-sudo docker run -d debian
-sudo docker run -d alpine
+# sudo docker run -d ubuntu
+# sudo docker run -d debian
+# sudo docker run -d alpine
 sudo docker ps -a
 sudo docker images
