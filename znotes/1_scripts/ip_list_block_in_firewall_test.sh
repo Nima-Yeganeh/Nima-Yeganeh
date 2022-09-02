0748bd18f87e7509beeba837908f5f86e0b09be4
root <yeganeh.nima.d@gmail.com>
2022-09-02 13:35:23 +0430

    done

--- znotes/1_scripts/ip_list_block_in_firewall_test.sh
+++ znotes/1_scripts/ip_list_block_in_firewall_test.sh
@@ -3,5 +3,6 @@ DROPPED_IPS="1.2.3.1 1.2.3.2 1.2.3.3"
 for ip in $DROPPED_IPS
 do
   echo "Dropping packets from $ip"
-  iptables -I INPUT -s $ip -j DROP
+  sudo iptables -I INPUT -s $ip -j DROP
 done
+sudo iptables --list | grep DROP
