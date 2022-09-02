d84ab0e986ab4f2b6f5817e1fb99ca05d76c8516
root <yeganeh.nima.d@gmail.com>
2022-09-02 13:05:21 +0430

    done

--- znotes/1_scripts/ping_dest_check_working.sh
+++ znotes/1_scripts/ping_dest_check_working.sh
@@ -4,6 +4,9 @@ echo "$output"
 if [[ "$output" == *"100% packet loss"* ]]
 then
   echo "not working!"
+elif [[ "$output" == *"address required"* ]]
+then
+  echo "not address! try again!"
 else
   echo "ok!"
 fi
