d79f257ba3bf0a2b8c58b69b8a95b3b3197ddd90
root <yeganeh.nima.d@gmail.com>
2022-09-03 11:15:04 +0430

    done

--- znotes/1_scripts/docker_httpd_test.sh
+++ znotes/1_scripts/docker_httpd_test.sh
@@ -1,4 +1,7 @@
-docker rm -f myhttpdsite1 myhttpdsite2
+docker ps
+docker rm -f myhttpdsite1
+docker rm -f myhttpdsite2
 docker container run -d -p 8081:80 --name myhttpdsite1 httpd
 docker container run -d -p 8082:80 --name myhttpdsite2 httpd
 docker ps
+ip a | grep inet
