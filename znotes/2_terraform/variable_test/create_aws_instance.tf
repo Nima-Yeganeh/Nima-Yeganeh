aa59ac9907d2b1b90b10be34071c5dd0d5dbeedc
root <yeganeh.nima.d@gmail.com>
2022-09-05 18:54:31 +0430

    done

--- znotes/2_terraform/variable_test/create_aws_instance.tf
+++ znotes/2_terraform/variable_test/create_aws_instance.tf
@@ -1,8 +1,10 @@
 resource "aws_instance" "MyThirdInstance" {
-    ami             = ""
-    instance_type   = "t2.micro"
-    
+    ami             = lookup(var.AMIS, var.AWS_REGION)
+    instance_type   = "t2.micro"  
+
     tags = {
         Name = "demo_instance"
     }
+
+    security_groups = var.Security_Group
 }
