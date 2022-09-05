b980666b9c86160d6272a847f8ee275ef184883e
root <yeganeh.nima.d@gmail.com>
2022-09-05 18:56:18 +0430

    done

--- znotes/2_terraform/variable_test/variables.tf
+++ znotes/2_terraform/variable_test/variables.tf
@@ -1,18 +1,22 @@
 variable "AWS_ACCESS_KEY" {}
+
 variable "AWS_SECRET_KEY" {}
+
 variable "AWS_REGION" {
     default = "us-east-2"
 }
+
 variable "Security_Group" {
-    type    = list
+    type = list
     default = ["sg-1, "sg-2", "sg-3"]
 }
+
 variable "AMIS" {
-    type    = map
+    type = map
     default = {
-        us-east-1   = "ami-1"
-        us-east-2   = "ami-2"
-        us-west-1   = "ami-3"
-        us-west-2   = "ami-4"
+        us-east-1 = "ami-1"
+        us-east-2 = "ami-2"
+        us-west-1 = "ami-3"
+        us-west-2 = "ami-4"
     }
 }
