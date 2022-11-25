198ee9cd8cf2ac02ab57d1c2b8374c4962ab1211
root <yeganeh.nima.d@gmail.com>
2022-11-25 13:23:09 +0330

    done

--- wordpress/test7/test.sh
+++ wordpress/test7/test.sh
@@ -1,5 +1,12 @@
 docker-compose run --rm wpcli1 sh
 wp config list
+
+wp plugin delete akismet
+wp plugin delete hello
+wp plugin install disable-comments --activate
+wp disable-comments delete --types=all
+wp disable-comments settings --types=all
+
 wp config set WP_REDIS_HOST redis1
 wp config set WP_CACHE_KEY_SALT wp-docker-5DknvYepdjyJMo8gDqrLhrpAJUQ
 wp config set WP_REDIS_PORT 6379
@@ -12,11 +19,17 @@ wp plugin install redis-cache --activate
 wp redis status
 wp redis enable --force
 
-wp plugin delete akismet
-wp plugin delete hello
+
 wp config set EP_HOST http://elk1:9200
 wp plugin install elasticpress --activate
-wp elasticpress index --setup
+
+wp elasticpress activate-feature autosuggest
+wp elasticpress activate-feature terms
+wp elasticpress clear-index
+wp elasticpress delete-index --yes
+wp elasticpress list-features
+wp elasticpress index --setup --yes
+
 
 
 docker container exec -it test6-redis1-1 sh
