349f53881ce12690e9492c54195654cf2350ad69
root <yeganeh.nima.d@gmail.com>
2022-12-01 14:43:22 +0330

    done

---
+++ php/docker-nginx-php-mysql-test1/web/public/register/index.php
@@ -0,0 +1,12 @@
+
+<?php
+?><!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Docker <?php echo "Hello World!"; ?></title>
+    </head>
+    <body>
+        <h1>Docker <?php echo "Hello World!"; ?></h1>
+    </body>
+</html>
