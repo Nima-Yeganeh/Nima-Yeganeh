68bc335534fb32c29e65e7f810f63173fd05ad68
root <yeganeh.nima.d@gmail.com>
2022-12-09 10:04:41 +0330

    done

---
+++ node.js/test1/demo_create_mysql_table.js
@@ -0,0 +1,18 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "wordpress",
+  password: "wordpress",
+  database: "mydb"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Table created");
+  });
+});
