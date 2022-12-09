79b7cc8e7e3e43d329e0e7158b1c0ad956d39856
root <yeganeh.nima.d@gmail.com>
2022-12-09 11:24:47 +0330

    done

---
+++ node.js/test1/demo_mysql_db_delete.js
@@ -0,0 +1,17 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "wordpress",
+  database: "mydb"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  var sql = "DELETE FROM customers WHERE address = 'Highway 37'";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Number of records deleted: " + result.affectedRows);
+  });
+});
